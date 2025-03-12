394db15eb0ef217ce6da253fe91bbdafe41c81fa
yuliya rothman <yuliya.nedyalkova777@gmail.com>
2025-03-12 19:35:49 +0000

    Simplification and basic error handling

--- day_25_us_states_game_start/main.py
+++ day_25_us_states_game_start/main.py
@@ -12,20 +12,23 @@ turtle.shape(image)
 correct_states = []
 map_manager = MapManager()
 
-is_game_on = True
-while is_game_on:
+
+while len(correct_states) < 50:
     # Answer management
-    answer_state = screen.textinput('Guess the State',"What's another name of a state?").title()
-    # Answer Management
-    ## check answer against the list of states
-    states_data = pd.read_csv('50_states.csv')
-    correct_answer = states_data[states_data.state == answer_state]
-    ## if correct write on the map
-    map_manager.update_map(correct_answer)
-    correct_states.append(correct_answer)
-
-
-
+    guessed_states = len(correct_states)
+    answer_state = screen.textinput(f"Guess the State({guessed_states}/50 States)",
+                                        "What's another name of a state?").title()
+    if answer_state:
+        # Answer Management
+        states_data = pd.read_csv('50_states.csv')
+        try:
+            ## check answer against the list of states
+            correct_answer = states_data[states_data.state == answer_state]
+            correct_states.append(correct_answer)
+            ## if correct write on the map
+            map_manager.update_map(correct_answer)
+        except ValueError as e:
+            print(e)
 
 
 screen.exitonclick()
