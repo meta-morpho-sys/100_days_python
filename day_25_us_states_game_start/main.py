8863ca67f70321c682fa96d2219ecfc198c89dd5
yuliya rothman <yuliya.nedyalkova777@gmail.com>
2025-03-13 16:43:51 +0000

    Turned into a learning game

--- day_25_us_states_game_start/main.py
+++ day_25_us_states_game_start/main.py
@@ -9,7 +9,7 @@ image = 'blank_states_img.gif'
 screen.addshape(image)
 turtle.shape(image)
 
-correct_states = []
+correct_states = set([])
 map_manager = MapManager()
 states_data = pd.read_csv('50_states.csv')
 all_states = states_data.state.to_list()
@@ -21,9 +21,14 @@ while len(correct_states) < 50:
     answer_state = screen.textinput(f"Guess the State({guessed_states}/50 States)",
                                     "What's another name of a state?").title()
     if answer_state == 'Exit':
+        # Get all the names of the states which haven't been guessed
+        states_to_learn = set(all_states) - correct_states
+        new_data = pd.DataFrame(states_to_learn)
+        # Write them in a `states_to_learn.csv`
+        new_data.to_csv('states_to_learn.csv')
         break
     if answer_state in all_states:
         ## check answer against the list of states
-        correct_states.append(answer_state)
+        correct_states.add(answer_state)
         state_data_row = states_data[states_data.state == answer_state]
         map_manager.update_map(state_data_row)
