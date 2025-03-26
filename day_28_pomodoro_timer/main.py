0a84a8653aa90ab92635dc1877cc811e1e477b39
yuliya rothman <yuliya.nedyalkova777@gmail.com>
2025-03-26 22:01:26 +0000

    Countdown mechanism

--- day_28_pomodoro_timer/main.py
+++ day_28_pomodoro_timer/main.py
@@ -14,7 +14,10 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-
+def count_down(count, time_text):
+    if count >= 0:
+        canvas.itemconfig(time_text, text=count)
+        canvas.after(1000, count_down, count - 1, time_text)
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title('Pomodoro')
@@ -23,8 +26,9 @@ window.config(pady=50, padx=100, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file='tomato.png')
 canvas.create_image(99, 112, image=tomato_img)
-canvas.create_text(99,130, text='00:00', fill='white', font=(FONT_NAME, 31, 'bold'))
+timer_text = canvas.create_text(99,130, text='00:00', fill='white', font=(FONT_NAME, 31, 'bold'))
 canvas.grid(column=1, row=1)
+count_down(5, timer_text)
 
 # Buttons
 start_button = Button(text='Start', width=3, bg=YELLOW, border=0, highlightthickness=0)
