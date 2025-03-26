9e5ed1c2421fdc47d38302b47c0b0610553455e6
yuliya rothman <yuliya.nedyalkova777@gmail.com>
2025-03-26 22:17:22 +0000

    Countdown mechanism

--- day_28_pomodoro_timer/main.py
+++ day_28_pomodoro_timer/main.py
@@ -11,13 +11,16 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_count():
+    count_down(5)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-def count_down(count, time_text):
+def count_down(count):
     if count >= 0:
-        canvas.itemconfig(time_text, text=count)
-        canvas.after(1000, count_down, count - 1, time_text)
+        canvas.itemconfig(timer_text, text=count)
+        canvas.after(1000, count_down, count - 1)
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title('Pomodoro')
@@ -28,10 +31,10 @@ tomato_img = PhotoImage(file='tomato.png')
 canvas.create_image(99, 112, image=tomato_img)
 timer_text = canvas.create_text(99,130, text='00:00', fill='white', font=(FONT_NAME, 31, 'bold'))
 canvas.grid(column=1, row=1)
-count_down(5, timer_text)
+
 
 # Buttons
-start_button = Button(text='Start', width=3, bg=YELLOW, border=0, highlightthickness=0)
+start_button = Button(text='Start', width=3, bg=YELLOW, border=0, highlightthickness=0, command=start_count)
 start_button.grid(column=0, row=2)
 reset_button = Button(text='Reset', width=3, bg=YELLOW, border=0, highlightthickness=0)
 reset_button.grid(column=2, row=2)
