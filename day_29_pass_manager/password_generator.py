1b0b91c8b55ee357c6c3eac299a064cbc3bce8dc
yuliya rothman <yuliya.nedyalkova777@gmail.com>
2025-03-31 12:52:36 +0100

    Password generating code

--- day_29_pass_manager/password_generator.py
+++ day_29_pass_manager/password_generator.py
@@ -5,12 +5,13 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 # --------------------- Mechanism to Pick Letters, Numbers and Symbols ------------------
-letters_to_pick = random.randint(8, 10)
-numbers_to_pick = random.randint(4,6)
+#  1st layer of randomness: We decide how many letters/numbers/symbols the code will pick for the password
+letters_to_pick = random.randint(8,10)
+numbers_to_pick = random.randint(2,4)
 symbols_to_pick = random.randint(2,4)
-print(letters_to_pick, numbers_to_pick,symbols_to_pick)
 
 # --------------------- Mechanism to randomly append them to a characters list ----------
+# 2nd layer of randomness: We use the random integers from above as a range for the random choice from relative array
 password_chars_list = []
 
 [password_chars_list.append(random.choice(letters)) for char in range(letters_to_pick)]
@@ -18,8 +19,7 @@ password_chars_list = []
 [password_chars_list.append(random.choice(symbols)) for sym in range(symbols_to_pick)]
 
 # --------------------- Mechanism to shuffle the chars and output a password ------------
-random.shuffle(password_chars_list)
-print(password_chars_list)
+# 3rd layer of randomness - shuffling the characters before transforming them into a password string
 
 password = ''
 for char in password_chars_list:
