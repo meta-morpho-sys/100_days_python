906756b4d0d3f343c0ac4b937683e5102acee7ac
yuliya rothman <yuliya.nedyalkova777@gmail.com>
2025-03-31 18:00:58 +0100

    Improved with error handling

--- day_26/NATO-alphabet-start/main.py
+++ day_26/NATO-alphabet-start/main.py
@@ -9,7 +9,16 @@ data = pandas.read_csv('nato_phonetic_alphabet.csv')
 nato_phonetic_dict = {row.letter:row.code for (index,row) in data.iterrows() }
 
 # 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input('Enter a name. No white spaces for now :-) :').upper()
-phon_words_list = [nato_phonetic_dict[letter] for letter in user_input]
-print(phon_words_list)
+def generate_phonetic_spelling():
+    try:
+        user_input = input('Enter a name. No white spaces for now :-) :').upper()
+        phon_words_list = [nato_phonetic_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters of the Alphabet, please")
+        generate_phonetic_spelling()
+    else:
+        print(phon_words_list)
+
+generate_phonetic_spelling()
+
 
