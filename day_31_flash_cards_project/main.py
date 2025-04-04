81f59cd3f1ff844144f87ecf691d6150e57d58e0
yuliya rothman <yuliya.nedyalkova777@gmail.com>
2025-04-04 18:37:23 +0100

    Picks randomly a coupled origin-destination words

--- day_31_flash_cards_project/main.py
+++ day_31_flash_cards_project/main.py
@@ -1,8 +1,19 @@
+import random
 from tkinter import *
+import pandas as pd
+from random import *
 
 BACKGROUND_COLOR = "#B1DDC6"
 LANGUAGE_FONT = ('Arial', 30, 'italic')
 WORD_FONT = ('Arial', 60, 'bold')
+
+# ---------------------------- MECHANISM TO DISPLAY WORDS ------------------------------- #
+
+data = pd.read_csv('data/french_words.csv')
+
+def pick_a_word():
+    coupled_words = data.to_dict(orient='records')
+    print(coupled_words[randint(0, len(coupled_words))])
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -21,9 +32,12 @@ canvas.grid(row=0,column=0,columnspan=2)
 # Buttons
 right_image = PhotoImage(file='images/right.png')
 wrong_image = PhotoImage(file='images/wrong.png')
-yes_button = Button(image=right_image, highlightthickness=0, highlightbackground=BACKGROUND_COLOR)
+yes_button = Button(image=right_image, highlightthickness=0, highlightbackground=BACKGROUND_COLOR, command=pick_a_word)
 yes_button.grid(row=1,column=1)
 
-no_button = Button(image=wrong_image, highlightthickness=0, highlightbackground=BACKGROUND_COLOR)
+no_button = Button(image=wrong_image, highlightthickness=0, highlightbackground=BACKGROUND_COLOR, command=pick_a_word)
 no_button.grid(row=1,column=0)
+
+
+
 window.mainloop()
