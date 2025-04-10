33402714ad5cd375f341a832ef1623ad47a4f842
yuliya rothman <yuliya.nedyalkova777@gmail.com>
2025-04-10 12:03:34 +0100

    Prep bday message

--- day_32_bday_wisher/birthday-wisher-extrahard-start/main.py
+++ day_32_bday_wisher/birthday-wisher-extrahard-start/main.py
@@ -10,9 +10,13 @@
 
 
 import datetime as dt
+import os
+import random
 import pandas as pd
 
+name = ''
 today = dt.date.today()
+year = today.year
 
 # ------------------------------ PREP DATA ------------------------------ #
 birthdays = pd.read_csv('birthdays.csv')
@@ -20,21 +24,41 @@ birthdays_dict = birthdays.to_dict(orient="records")
 
 # ------------------------------ CHECK IF IT IS SOMEONE'S BIRTHDAY ------------------------------ #
 def birthday_check():
+    global name
     for entry in birthdays_dict:
         name = entry['name']
         month = entry['month']
         day = entry['day']
-        birthday = dt.date(today.year, month=month, day=day)
+        birthday = dt.date(year=year, month=month, day=day)
         if birthday == today:
-            print(f"HORRAY!! {name}'s birthday is on the: {birthday}")
-            return birthday
+            print(f"Wooohoo!! It's {name}'s birthday today the {birthday}")
+        return birthday, name, year
 
 
 # ------------------------------ PREP BIRTHDAY MESSAGE ------------------------------ #
+def write_message():
+    templates = os.listdir('letter_templates')
+    template = random.choice(templates)
+
+    with open(f"letter_templates/{template}") as template_text:
+        text = template_text.read()
+
+    letters_dir = 'ready_letters'
+    if not os.path.exists(letters_dir):
+        os.makedirs(letters_dir)
+    if not os.path.exists(f"{letters_dir}/{year}"):
+        os.makedirs(f"{letters_dir}/{year}")
+    else:
+        with open(f"{letters_dir}/{year}/{name}_b_day", 'w') as bday_letter:
+            new_text = text.replace('[NAME]', name)
+            bday_letter.write(new_text)
 
 
 
 # ------------------------------ SEND BIRTHDAY EMAIL ------------------------------ #
 
 
+
 birthday_check()
+write_message()
+
