b785a59e1a65444a041b96178bd618e0162793f2
yuliya rothman <yuliya.nedyalkova777@gmail.com>
2025-04-16 17:30:24 +0100

    Adding logic from previous lessons

--- day_34_quizzier/data.py
+++ day_34_quizzier/data.py
@@ -8,5 +8,5 @@ parameters = {
 response = requests.get("https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 question_data = response.json()['results']
-print(question_data)
+
 
