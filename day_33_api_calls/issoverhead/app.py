feb3b1ff1c5e89313ea62f0d40328be213a1b339
yuliya rothman <yuliya.nedyalkova777@gmail.com>
2025-04-16 17:10:47 +0100

    Improved UI

--- day_33_api_calls/issoverhead/app.py
+++ day_33_api_calls/issoverhead/app.py
@@ -10,10 +10,61 @@ server = app.server
 
 
 app.layout = html.Div([
-    html.H1("🛰️ ISS Live Tracker", style={"textAlign": "center"}),
+    html.H1("🛰️ ISS Live Tracker", style={"textAlign": "center", "color": "#444",}),
+    html.Div([
+         html.Button("ℹ️ ISS Info", id="toggle-info", n_clicks=0, style={
+        "position": "absolute",
+        "top": "20px",
+        "left": "20px",
+        "zIndex": 11
+    }),
+        # The info box (initially hidden)
+        html.Div([
+            html.H4("🛰️ ISS Tracker", style={"marginBottom": "5px"}),
+            html.P(
+                (
+                    "The International Space Station (ISS) is a large space station that was assembled and is maintained in low Earth orbit by a collaboration of five space agencies and their contractors: NASA (United States), Roscosmos (Russia), ESA (Europe), JAXA (Japan), and CSA (Canada). "
+                    "As the largest space station ever constructed, it primarily serves as a platform for conducting scientific experiments in microgravity and studying the space environment. (Source: Wikipedia)"
+                ),
+                style={"fontSize": "12px", "lineHeight": "1.5"}
+            ),
+            html.Ul([
+                html.Li("Speed on orbit: 7.66 km/s"),
+                html.Li("Max speed: 28,000 km/h"),
+                html.Li("Orbit height: 408 km"),
+                html.Li("Launch date: 20 November 1998")
+            ],style={"fontSize": "12px", "lineHeight": "1.5"})
+        ],
+        id="info-box",
+        style={
+            "display": "none",
+            "position": "absolute",
+            "top": "70px",
+            "left": "20px",
+            "backgroundColor": "#f8f8f8",
+            "padding": "15px",
+            "border": "1px solid #ccc",
+            "borderRadius": "8px",
+            "width": "300px",
+            "zIndex": 10
+        })
+    ]),
+
     dcc.Graph(id='iss-map'),
-    html.H4("This map shows the real-time location  of the International Space Station (ISS).\n Updates every 60 seconds.",
-            style={"textAlign": "center", "marginTop": "20px"}),
+    html.H5("This map shows the real-time location  of the International Space Station (ISS).\n Updates every 60 seconds.",
+            style={"textAlign": "center", "color": "red", "marginTop": "20px"}),
+    html.P(
+        "It is part of an app which tracks whether the International Space Station (ISS) is currently flying over your location at night and during clear skies — and notifies you via email when the conditions are just right to observe it.",
+        style={
+            "textAlign": "center",
+            "maxWidth": "700px",
+            "margin": "20px auto",
+            "color": "#444",
+            "fontSize": "14px",
+            "lineHeight": "1.6"
+        }
+    ),
+
     dcc.Interval(id='interval-component', interval=60*1000, n_intervals=0)  # update every 60 seconds
 ])
 
@@ -36,7 +87,7 @@ def update_iss_location(n):
         lon=[lon],
         mode='text',
         text=["🛰"],
-        textfont=dict(size=38),
+        textfont=dict(size=42),
         name="ISS"
     ))
 
@@ -47,14 +98,43 @@ def update_iss_location(n):
             landcolor='rgb(243, 243, 243)',
             showcountries=True,
             countrywidth=1,
-            countrycolor='red',
+            countrycolor='#8B0000',
+            showocean=True,
+            oceancolor="#9de5f5",
             lataxis_range=[-90, 90],
             lonaxis_range=[-180, 180],
+            projection_type='natural earth'
         ),
         margin={"r":0,"t":0,"l":0,"b":0},
         height=600
     )
     return fig
 
+
+@app.callback(
+    Output("info-box", "style"),
+    Input("toggle-info", "n_clicks"),
+)
+def toggle_info_box(n_clicks):
+    base_style = {
+        "position": "absolute",
+        "top": "70px",
+        "left": "20px",
+        "backgroundColor": "#f8f8f8",
+        "padding": "15px",
+        "border": "1px solid #ccc",
+        "borderRadius": "8px",
+        "width": "300px",
+        "zIndex": 10
+    }
+
+    if n_clicks and n_clicks % 2 == 1:
+        # Show the box
+        return {**base_style, "display": "block"}
+    else:
+        # Hide the box
+        return {**base_style, "display": "none"}
+
+
 if __name__ == "__main__":
-    app.app.run(debug=True, port=8051)
+    app.run(debug=True, port=8051)
