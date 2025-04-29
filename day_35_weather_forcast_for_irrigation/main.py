941a14c0770d17509e8fc244daefa86ad1232b12
yuliya rothman <yuliya.nedyalkova777@gmail.com>
2025-04-29 15:21:20 +0100

    Base for Software driven irrigation system

---
+++ day_35_weather_forcast_for_irrigation/main.py
@@ -0,0 +1,32 @@
+import requests
+import os
+
+# Cinquefrondi,IT
+# MY_LAT = 38.418129
+# MY_LONG = 16.094990
+# Witney,UK
+MY_LAT = 51.789018
+MY_LONG = -1.484935
+weather_api_key = os.getenv('OPEN_WEATHER_API_KEY')
+weather_forecast_url = "https://api.openweathermap.org/data/2.5/forecast"
+condition_codes = []
+params = {
+    'lat': MY_LAT,
+    'lon': MY_LONG,
+    'appid': weather_api_key,
+    'units': 'metric',
+    'cnt': 4
+}
+
+
+# CHECK IF THE WEATHER FORCAST IS SUNNY
+response = requests.get(weather_forecast_url, params=params)
+response.raise_for_status()
+twelve_hour_weather_data = response.json()
+weather_forecast = twelve_hour_weather_data['list']
+[condition_codes.append(forcast['weather'][0]['id']) for forcast in weather_forecast]
+dry_weather_codes = [800, 801, 802]
+if any(code < 700 for code in condition_codes):
+    print("No need to water the plants")
+else:
+    print("Water the plants")
